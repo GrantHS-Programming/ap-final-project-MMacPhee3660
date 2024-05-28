0fb1dcb1d2d8defecef28397f668264892055106
mmacphee3660 <mmacphee3660@student.pps.net>
2024-05-28 11:38:48 -0700

    wheeeeee

--- FinalProject.py
+++ FinalProject.py
@@ -1,33 +1,82 @@
-import pygame
+import pgzrun
 
-pygame.init()
+WIDTH = 300
+HEIGHT = 300
 
-WIDTH = 500
-HEIGHT = 500
+star = Actor('star', (150, 150))
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-player = pygame.Rect((300, 250, 50, 50))
+heart = Actor('heart', (250, 250))
 
+bubble = Actor('bubble', (255, 230))
+bubble_hit = Rect((0, 0), (32, 32))
+bubble_hit.center = bubble.center
 
-
-run = True
-while run:
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen, (255, 0, 0), player)
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
-    elif key[pygame.K_d] == True:
-        player.move_ip(1, 0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0, -1)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, 1)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.display.update()
+window = Actor('window', (150, 150))
 
 
+x = Actor('x', (100, 100))
+x_hit = Rect((0, 0), (32,32))
+x_hit.center = x.center
 
-pygame.quit()
+background = 1
+click = 0
+
+
+def draw():
+    global background
+    screen.fill((255, 255, 255))
+
+    if background == 1:
+        screen.blit("test background 1", (150, 150))
+        heart.draw()
+        if star.top > WIDTH:
+            background = 2
+            star.top = 0
+        if star.distance_to(heart) < 40:
+            if click != 1:
+                bubble.draw()
+        if click == 1:
+            window.draw()
+            x.draw()
+
+    if background == 2:
+        screen.blit("test background 2", (150, 150))
+        if star.bottom < 0:
+            background = 1
+            star.bottom = WIDTH
+    star.draw()
+
+
+
+def update():
+    if click != 1:
+        if keyboard.w or keyboard.up:
+            star.y -= 1
+        if keyboard.s or keyboard.down:
+            star.y += 1
+        if keyboard.a or keyboard.left:
+            star.x -= 1
+        if keyboard.d or keyboard.right:
+            star.x += 1
+
+
+def on_mouse_down(pos):
+    global click
+    if bubble_hit.collidepoint(pos[0], pos[1]) and background == 1:
+        click = 1
+    if x_hit.collidepoint(pos[0], pos[1]) and background == 1:
+        click = 0
+
+
+def on_mouse_up():
+    global click
+
+
+
+
+
+
+
+
+
+pgzrun.go()
